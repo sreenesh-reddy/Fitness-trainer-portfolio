4abaa809b0236ef314d211633b2420bf5fb54ff6
sreenesh <sreenshreddy52@gmail.com>
2024-09-16 18:11:27 +0530

    bavani commit

--- src/app/components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (<div className="aspect-ratio w-full h-full">
-        <div className="flex w-full justify-center gap-8 text-2xl mt-7 font-thin">
-            <a href="">Home</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-        </div>
-    </div>)
-}
