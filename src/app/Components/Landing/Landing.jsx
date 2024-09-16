7e6f5ee6e786c00ba2783040bcee6d4cbfb5c145
sreenesh <sreenshreddy52@gmail.com>
2024-09-16 19:26:21 +0530

    lol

--- src/app/Components/Landing/Landing.jsx
+++ src/app/Components/Landing/Landing.jsx
@@ -7,15 +7,17 @@ export default function Landing() {
         <div className={styles.mainpage}>
             <div className={styles.hed}>
                 <h1>TRANSFORM YOUR BODY <br /> TRANSFORM YOUR LIFE</h1>
-                <div className={styles.button}>
-                    <a href="contact">CONTACT</a>
-                </div>
+                <a href="contact">
+                    <div className={styles.button}>
+                        CONTACT
+                    </div>
+                </a>
             </div>
             <div className={styles.img}>
-                <Image src={profile}
+                {/* <Image src={profile}
                     alt="VINAY"
                     width={600} height={500}>
-                </Image>
+                </Image> */}
             </div>
         </div>
     </>)
