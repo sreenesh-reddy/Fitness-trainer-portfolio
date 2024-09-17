81c0b0275adbc31a9e97202e37ac6cf21ff51160
sreenesh <sreenshreddy52@gmail.com>
2024-09-17 11:52:28 +0530

    added transformations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Landing from "./Components/Landing/Landing"
-
+import Transformations from "./Components/Transformations/Transformations"
 
 export default function Home() {
     return(
-        <>
-   
+        <div className="Home">
         <Landing/>
-        </>
+        <Transformations/>
+        </div>
     )
 }
 
