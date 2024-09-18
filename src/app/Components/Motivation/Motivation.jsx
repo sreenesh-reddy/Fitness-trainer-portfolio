40d72c12c0d88cdf7bb9b2cac988ac4ed87f4625
sreenesh <sreenshreddy52@gmail.com>
2024-09-18 11:52:13 +0530

    added heading

--- src/app/Components/Motivation/Motivation.jsx
+++ src/app/Components/Motivation/Motivation.jsx
@@ -4,10 +4,11 @@ import pic from './pic.png'
 export default function Motivaiton() {
     return (<div className={styles.body}>
         <div className={styles.mainpage}>
+        <h3>MY MOTIVATION</h3>
         <h1>
             NO PAIN <br />
             <div className={styles.imgcontainer}>
-                <Image src={pic} width={350} height={300} alt="picture unavailable" />
+                <Image src={pic} width={350} height={300} style={{borderRadius:"50px"}} alt="picture unavailable" />
             </div>
             NO GAIN
         </h1>
