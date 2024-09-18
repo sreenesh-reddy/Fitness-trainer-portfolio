44ac104ee7cab672a05808a0326a8989b852d8a7
sreenesh <sreenshreddy52@gmail.com>
2024-09-18 11:46:53 +0530

    motivation

---
+++ src/app/Components/Motivation/Motivation.jsx
@@ -0,0 +1,16 @@
+import styles from './style.module.scss'
+import Image from 'next/image'
+import pic from './pic.png'
+export default function Motivaiton() {
+    return (<div className={styles.body}>
+        <div className={styles.mainpage}>
+        <h1>
+            NO PAIN <br />
+            <div className={styles.imgcontainer}>
+                <Image src={pic} width={350} height={300} alt="picture unavailable" />
+            </div>
+            NO GAIN
+        </h1>
+        </div>
+    </div>)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Contact from "./Components/Contact/Contact"
 import Landing from "./Components/Landing/Landing"
 import Transformations from "./Components/Transformations/Transformations"
-
+import Motivation from "./Components/Motivation/Motivation"
 export default function Home() {
     return(
         <>
         <Landing/>
         {/* <Contact/> */}
+        <Motivation/>
         </>
         
     )
