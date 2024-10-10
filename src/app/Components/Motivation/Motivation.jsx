d7dc264d44f0369bf5783c90a3e345fb42b43547
sreenesh <sreeneshreddy52@gmail.com>
2024-10-10 13:54:18 +0530

    checkin

--- src/app/Components/Motivation/Motivation.jsx
+++ src/app/Components/Motivation/Motivation.jsx
@@ -3,23 +3,49 @@ import Image from 'next/image'
 import pic from './pic.png'
 
 export default function Motivaiton() {
-    return (<div className={styles.body}>
-        <div className={styles.mainpage}>
-        <div className={styles.heading}>
-        <h3>MOTIVATION</h3>
-        </div>
-        <h1>
-            NO <span>PAIN</span> <br />
-            <div className={styles.imgcontainer}>
-                <Image src={pic} width={350} height={300} style={{borderRadius:"50px"}} alt="picture unavailable" />
-            </div>
-            NO <span>GAIN</span>
-        </h1>
-        <a href="#About">
+    return (
+        <><div className={styles.pcbody}>
+            <div className={styles.mainpage}>
+                <div className={styles.heading}>
+                    <h3>MOTIVATION</h3>
+                </div>
+                <div className={styles.bod}>
+                <div className={styles.hed}>
+                <h1>
+                    NO <span>PAIN</span> <br />
+                    NO <span>GAIN</span>
+                </h1>
+                <a href="#About">
                     <div className={styles.button}>
                         ABOUT
                     </div>
-        </a>
+                </a>
+                </div>
+                <div className={styles.imgcontainer}>
+                        <Image src={pic} width={350} height={300} style={{ borderRadius: "50px" }} alt="picture unavailable" />
+                </div>
+                </div>
+            </div>
         </div>
-    </div>)
+
+            <div className={styles.mobbody}>
+                <div className={styles.mainpage}>
+                    <div className={styles.heading}>
+                        <h3>MOTIVATION</h3>
+                    </div>
+                    <h1>
+                        NO <span>PAIN</span> <br />
+                        <div className={styles.imgcontainer}>
+                            <Image src={pic} width={350} height={300} style={{ borderRadius: "50px" }} alt="picture unavailable" />
+                        </div>
+                        NO <span>GAIN</span>
+                    </h1>
+                    <a href="#About">
+                        <div className={styles.button}>
+                            ABOUT
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </>)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         <Landing/>
         <Motivation/>
         <Transformations/>
-        <About/>
         <Contact/>  
         
         
